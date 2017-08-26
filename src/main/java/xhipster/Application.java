90f0d659db1ccf4e70fe177d9c813c741fb10372
Andrei <danandreist@gmail.com>
2017-08-27 00:08:29 +0300

    docker ip:port

--- src/main/java/xhipster/Application.java
+++ src/main/java/xhipster/Application.java
@@ -3,7 +3,6 @@ package xhipster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
@@ -13,11 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws IOException {
-        ApplicationContext appContext = SpringApplication.run(Application.class, args);
+  public static void main(String[] args) throws IOException {
+    ApplicationContext appContext = SpringApplication.run(Application.class, args);
 
-        log.info(":::::::::::::::::::: Running on http://localhost:1337 ::::::::::::::::::::");
-        log.info(":::::::::::::::::::: on Spring "+  org.springframework.core.SpringVersion.getVersion() +" ::::::::::::::::::::");
-
-    }
+    log.info(":::::::::::::::::::: Spring " + org.springframework.core.SpringVersion.getVersion() + " ::::::::::::::::::::");
+    log.info(":::::::::::::::::::: Running on http://localhost:1337 ::::::::::::::::::::");
+  }
 }
